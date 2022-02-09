8ce9cb775f16a2b02069d12d1be8b6ede4d50e08
fedniko <fednik2011@gmail.com>
2022-02-09 13:05:21 +0300

    #23 add reminder in strings

--- strings.py
+++ strings.py
@@ -1,4 +1,21 @@
-from aiogram.utils.markdown import text
+from aiogram.utils.markdown import text, bold
 
 help_command = 'help'
 help_text = text('**Это помощь :D**')
+
+
+class Reminder:
+    def __init__(self, command, description):
+        self.command = command
+        self.description = description
+
+
+# @todo #3 перевести в Enum
+reminders_start = {
+    'schedule': Reminder('schedule', text(f"Необходимо внести изменения в {bold('Google Calendar')}.",
+                                          "Данное сообщение будет повторяться каждые 15 минут.",
+                                          "Для отключения необходимо ввести /done_schedule.", sep='\n')),
+    'homework': Reminder('homework', text("Необходимо внести изменения в Trello.",
+                                          "Данное сообщение будет повторяться каждые 15 минут.",
+                                          "Для отключения необходимо ввести /done_homework", sep='\n'))
+}
